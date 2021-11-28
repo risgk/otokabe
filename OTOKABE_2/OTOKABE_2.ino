a002b2e9ae08c181d53ad1b561d7b6464ea1d4b1
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2021-11-28 09:55:39 +0900

    Update OTOKABE_2.ino

--- OTOKABE_2/OTOKABE_2.ino
+++ OTOKABE_2/OTOKABE_2.ino
@@ -12,46 +12,46 @@
 #include <MIDIUSB.h>
 #endif
 
-#define NOTE_ON_VELOCITY         (100)
-#define DEFAULT_MIDI_CH          (0)
-#define ANTICHATTERING_WAIT_MSEC (100)
-#define INVALID                  (0xFF)
+#define NOTE_ON_VELOCITY            (100)
+#define DEFAULT_CHANNEL_ZERO_ORIGIN (0)
+#define ANTICHATTERING_WAIT_MSEC    (100)
+#define INVALID                     (0xFF)
 
 typedef struct {
-  int          analogThreshold;  // Analog threshold
-  byte         value;            // HIGH or LOW
-  unsigned int valueChangedTime; // msec
-  byte         midiChZeroOrigin; // 0 to 15
-  byte         noteNumber;       // 0 to 127 (INVALID if there is no sensor)
+  int          analogThreshold;          // Analog threshold
+  byte         digitalValue;             // HIGH or LOW
+  unsigned int digitalValueChangedTime;  // msec
+  byte         channelZeroOrigin;        // 0 to 15
+  byte         noteNumber;               // 0 to 127 (INVALID if there is no sensor)
 } SENSOR_STATE;
 
 SENSOR_STATE s_sensorStates[] = {
 #if (BOARD_NUMBER == 0)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 60      }, // A0
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 61      }, // A1
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 62      }, // A2
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 63      }, // A3
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 64      }, // A4
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 65      }, // A5
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 66      }, // A6 (D4)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 67      }, // A7 (D6)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 68      }, // A8 (D8)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 69      }, // A9 (D9)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 70      }, // A10 (D10)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 71      }, // A11 (D12)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 60      }, // A0
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 61      }, // A1
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 62      }, // A2
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 63      }, // A3
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 64      }, // A4
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 65      }, // A5
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 66      }, // A6 (D4)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 67      }, // A7 (D6)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 68      }, // A8 (D8)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 69      }, // A9 (D9)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 70      }, // A10 (D10)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 71      }, // A11 (D12)
 #elif (BOARD_NUMBER == 1)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 48      }, // A0
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 49      }, // A1
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 50      }, // A2
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 51      }, // A3
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 52      }, // A4
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 53      }, // A5
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 54      }, // A6 (D4)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 55      }, // A7 (D6)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 56      }, // A8 (D8)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 57      }, // A9 (D9)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 58      }, // A10 (D10)
-  { 0, HIGH, 0, DEFAULT_MIDI_CH, 59      }, // A11 (D12)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 48      }, // A0
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 49      }, // A1
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 50      }, // A2
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 51      }, // A3
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 52      }, // A4
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 53      }, // A5
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 54      }, // A6 (D4)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 55      }, // A7 (D6)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 56      }, // A8 (D8)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 57      }, // A9 (D9)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 58      }, // A10 (D10)
+  { 0, HIGH, 0, DEFAULT_CHANNEL_ZERO_ORIGIN, 59      }, // A11 (D12)
 #endif
 };
 
@@ -88,17 +88,17 @@ void loop()
     if (s_sensorStates[analogPin].noteNumber != INVALID) {
       unsigned int currentTime = millis();
 
-      if (currentTime - s_sensorStates[analogPin].valueChangedTime >= ANTICHATTERING_WAIT_MSEC) {
-        byte sensorValue = readSensorValue(analogPin, s_sensorStates[analogPin].analogThreshold);
+      if (currentTime - s_sensorStates[analogPin].digitalValueChangedTime >= ANTICHATTERING_WAIT_MSEC) {
+        byte sensorValue = readSensorDigitalValue(analogPin, s_sensorStates[analogPin].analogThreshold);
 
-        if ((s_sensorStates[analogPin].value == HIGH) && (sensorValue == LOW)) {
-          s_sensorStates[analogPin].value = LOW;
-          s_sensorStates[analogPin].valueChangedTime = currentTime;
-          sendMIDINoteOn(s_sensorStates[analogPin].midiChZeroOrigin, s_sensorStates[analogPin].noteNumber, NOTE_ON_VELOCITY);
-        } else if ((s_sensorStates[analogPin].value == LOW) && (sensorValue == HIGH)) {
-          s_sensorStates[analogPin].value = HIGH;
-          s_sensorStates[analogPin].valueChangedTime = currentTime;
-          sendMIDINoteOff(s_sensorStates[analogPin].midiChZeroOrigin, s_sensorStates[analogPin].noteNumber, 64);
+        if ((s_sensorStates[analogPin].digitalValue == HIGH) && (sensorValue == LOW)) {
+          s_sensorStates[analogPin].digitalValue = LOW;
+          s_sensorStates[analogPin].digitalValueChangedTime = currentTime;
+          sendMIDINoteOn(s_sensorStates[analogPin].channelZeroOrigin, s_sensorStates[analogPin].noteNumber, NOTE_ON_VELOCITY);
+        } else if ((s_sensorStates[analogPin].digitalValue == LOW) && (sensorValue == HIGH)) {
+          s_sensorStates[analogPin].digitalValue = HIGH;
+          s_sensorStates[analogPin].digitalValueChangedTime = currentTime;
+          sendMIDINoteOff(s_sensorStates[analogPin].channelZeroOrigin, s_sensorStates[analogPin].noteNumber, 64);
         }
       }
     }
@@ -109,7 +109,7 @@ void loop()
 #endif
 }
 
-byte readSensorValue(byte analogPin, int analogThreshold)
+byte readSensorDigitalValue(byte analogPin, int analogThreshold)
 {
   int analogValue = analogRead(analogPin);
 
@@ -125,53 +125,53 @@ byte readSensorValue(byte analogPin, int analogThreshold)
   return LOW;
 }
 
-void sendMIDINoteOn(byte midiChZeroOrigin, byte noteNumber, byte velocity)
+void sendMIDINoteOn(byte channelZeroOrigin, byte noteNumber, byte velocity)
 {
-  MIDI.sendNoteOn(noteNumber, velocity, midiChZeroOrigin + 1);
+  MIDI.sendNoteOn(noteNumber, velocity, channelZeroOrigin + 1);
 
 #if defined(ENABLE_MIDIUSB)
-  midiEventPacket_t event = {0x09, (uint8_t) (0x90 | midiChZeroOrigin), noteNumber, NOTE_ON_VELOCITY};
+  midiEventPacket_t event = {0x09, (uint8_t) (0x90 | channelZeroOrigin), noteNumber, NOTE_ON_VELOCITY};
   MidiUSB.sendMIDI(event);
   MidiUSB.flush();
 #endif
 }
 
-void sendMIDINoteOff(byte midiChZeroOrigin, byte noteNumber, byte velocity)
+void sendMIDINoteOff(byte channelZeroOrigin, byte noteNumber, byte velocity)
 {
-  MIDI.sendNoteOff(noteNumber, velocity, midiChZeroOrigin + 1);
+  MIDI.sendNoteOff(noteNumber, velocity, channelZeroOrigin + 1);
 
 #if defined(ENABLE_MIDIUSB)
-  midiEventPacket_t event = {0x08, (uint8_t) (0x80 | midiChZeroOrigin), noteNumber, velocity};
+  midiEventPacket_t event = {0x08, (uint8_t) (0x80 | channelZeroOrigin), noteNumber, velocity};
   MidiUSB.sendMIDI(event);
   MidiUSB.flush();
 #endif
 }
 
-void sendMIDIControlChange(byte midiChZeroOrigin, byte controlNumber, byte value)
+void sendMIDIControlChange(byte channelZeroOrigin, byte controlNumber, byte value)
 {
-  MIDI.sendControlChange(controlNumber, value, midiChZeroOrigin + 1);
+  MIDI.sendControlChange(controlNumber, value, channelZeroOrigin + 1);
 
 #if defined(ENABLE_MIDIUSB)
-  midiEventPacket_t event = {0x0B, (uint8_t) (0xB0 | midiChZeroOrigin), controlNumber, value};
+  midiEventPacket_t event = {0x0B, (uint8_t) (0xB0 | channelZeroOrigin), controlNumber, value};
   MidiUSB.sendMIDI(event);
   MidiUSB.flush();
 #endif
 }
 
-void handlerNoteOn(byte channelOneOrigin, byte note, byte velocity)
+void handlerNoteOn(byte channelOneOrigin, byte noteNumber, byte velocity)
 {
   // Forwarding
-  sendMIDINoteOn(channelOneOrigin - 1, note, velocity);
+  sendMIDINoteOn(channelOneOrigin - 1, noteNumber, velocity);
 }
 
-void handlerNoteOff(byte channelOneOrigin, byte note, byte velocity)
+void handlerNoteOff(byte channelOneOrigin, byte noteNumber, byte velocity)
 {
   // Forwarding
-  sendMIDINoteOff(channelOneOrigin - 1, note, velocity);
+  sendMIDINoteOff(channelOneOrigin - 1, noteNumber, velocity);
 }
 
-void handlerControlChange(byte channelOneOrigin, byte note, byte value)
+void handlerControlChange(byte channelOneOrigin, byte noteNumber, byte value)
 {
   // Forwarding
-  sendMIDIControlChange(channelOneOrigin - 1, note, value);
+  sendMIDIControlChange(channelOneOrigin - 1, noteNumber, value);
 }
